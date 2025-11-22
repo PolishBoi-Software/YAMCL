fe1ca4261da781848cb12b6942c07336d75730ea
PolishBoiYT <polishboiyt@gmail.com>
2025-11-22 12:49:53 +0100

    feat: hello, yamcl website!

---
+++ index.js
@@ -0,0 +1,15 @@
+$(".discord-server-join").click(function() {
+    window.open("https://discord.gg/8KAtkftPDu", "_blank")
+});
+
+$(".gh-repo-view").click(function() {
+    window.open("https://github.com/PolishBoi-Software/Yet-Another-Minecraft-Launcher", "_blank")
+});
+
+$(".download").click(function() {
+    window.open("https://github.com/PolishBoi-Software/Yet-Another-Minecraft-Launcher/releases/latest/download/YAMCL-setup.exe", "_blank")
+});
+
+$(".changelog").click(function() {
+    window.open("https://github.com/PolishBoi-Software/Yet-Another-Minecraft-Launcher/releases/latest/", "_blank")
+});
